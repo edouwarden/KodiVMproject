4d0623fc3e1a27fd792273c644e734f1a6d15b35
edouwarden <121302385+edouwarden@users.noreply.github.com>
2022-12-23 16:08:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,11 @@ sudo apt install kodi lightdm -y
 sudo apt remove --purge
 sudo apt autoremove -y
 
+# Ajoutez la source de médias
+kodi-send --action="{\"jsonrpc\":\"2.0\",\"method\":\"Files.AddVideosource\",\"params\":{\"name\":\"Serveur Kodi\",\"path\":\"https://kodi.majyx.net/\",\"username\":\"student\",\"password\":\"student\"},\"id\":1}"
+
+# Obtenez la liste des sources de médias
+sources=$(kodi-send --action="{\"jsonrpc\":\"2.0\",\"method\":\"Files.GetSources\",\"params\":{\"media\":\"video
+
 
 echo "Le script a été exécuté avec succès"
